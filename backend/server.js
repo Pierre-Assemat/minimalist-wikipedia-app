1eba16c5d7613840f9a7e8ea8ee5b066a52beee3
Pierre-Assemat <pierre@k.solutions>
2019-11-14 18:41:35 +0100

    Add axios to perform external API request

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,57 @@
 const express = require('express');
-const path = require('path');
-const https = require('https');
+const axios = require('axios');
 
-// Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-// App
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
-});
-
-app.get('/wiki/summary', (req, res) => {
+app.get('/wiki/summary', (req, res, next) => {
   console.log('Fetching wikipedia article');
-  https.get('https://xkcd.com/614/info.0.json', res => {
-    console.log(res);
-  });
+  const title = req.query.title;
+
+  if (title) {
+    axios
+      .get(`https://en.wikipedia.org/api/rest_v1/page/summary/${title}`, {
+        headers: {
+          'User-Agent': 'app'
+        }
+      })
+      .then(result => {
+        res.send(result.data);
+      })
+      .catch(error => {
+        next(error);
+      });
+  } else {
+    res.send();
+  }
 });
 
 app.get('/wiki/pdf', (req, res) => {
   // TODO: Implement PDF get request
 });
 
-app.get('/wiki/XKCD', (req, res) => {
+app.get('/wiki/XKCD', (req, res, next) => {
   console.log("Fetching XKCD blog's last drawing");
+
+  axios
+    .get('https://xkcd.com/614/info.0.json')
+    .then(result => {
+      res.send(result.data.img);
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
+app.use(function(err, req, res, next) {
+  console.log(`Error: ${err.message}`);
+  res.status(err.response.status).json({
+    error: err.message
+  });
 });
 
 app.listen(PORT, HOST);
+
 console.log(`Running on http://${HOST}:${PORT}`);
