dacf8f08cf8d47b6eb7333ab7b6728b2a3b5e73b
Pierre-Assemat <pierre@k.solutions>
2019-11-15 10:44:13 +0100

    FrontEnd: Implement basic front using vue-cli

---
+++ frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>Minimalist wikipedia app</h1>
+    <wikipediaArticle/>
+  </div>
+</template>
+
+<script>
+import WikipediaArticle from './components/wikipediaArticle'
+
+export default {
+  name: 'App',
+  components: {
+    WikipediaArticle
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ frontend/src/components/wikipediaArticle.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="wiki" v-if="summary">
+    <h2>{{ summary.title }}</h2>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'WikipediaArticle',
+  data () {
+    return {
+      summary: null
+    }
+  },
+
+  mounted () {
+    axios.get('http://0.0.0.0:8000/wiki/summary/XKCD').then(response => {
+      if (response.status === 200) {
+        console.log(response.data)
+        this.summary = response.data || null
+      }
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+#wiki {
+  max-width: 800px;  
+}
+
+</style>
